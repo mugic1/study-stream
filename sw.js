7b8d3f0c11bdeece7e7006538f0528d7d036f6a1
mugic1 <dasandrew047@gmail.com>
2026-01-29 14:31:29 +0530

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'studystream-v1';
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll([
+          './',
+          './index.html',
+          'https://unpkg.com/lucide@latest',
+          'https://www.youtube.com/iframe_api'
+        ]);
+      })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        return response || fetch(event.request);
+      })
+  );
+});

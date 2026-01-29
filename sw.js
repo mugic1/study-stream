b4f34d7ac83a855285dd3759805389e1aa5f642f
mugic1 <dasandrew047@gmail.com>
2026-01-29 14:58:42 +0530

    Refactor service worker for caching assets

--- sw.js
+++ sw.js
@@ -1,24 +1,18 @@
-const CACHE_NAME = 'studystream-v1';
+const CACHE_NAME = 'study-stream-v1';
+const assets = [
+  '/study-stream/',
+  '/study-stream/index.html',
+  '/study-stream/manifest.json'
+];
 
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then((cache) => {
-        return cache.addAll([
-          './',
-          './index.html',
-          'https://unpkg.com/lucide@latest',
-          'https://www.youtube.com/iframe_api'
-        ]);
-      })
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(assets))
   );
 });
 
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request)
-      .then((response) => {
-        return response || fetch(event.request);
-      })
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
   );
 });
